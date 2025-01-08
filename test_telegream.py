cab4557d47b5325299db09b51d7bbd8e8ce4fdaa
VaryoSc <waryor99@gmail.com>
2025-01-08 09:45:59 +0330

    Uploading the safe version🛟

--- test_telegream.py
+++ test_telegream.py
@@ -13,94 +13,97 @@ def get_profile(users: List[str], driver=None):
     if not driver:
         driver = use_driver()
 
-    picture_class = "profile-avatars-avatar"
-    bio_class = "row-title pre-wrap"
     file_name = ("telegram"
                  + time.strftime("%Y%m%d", time.localtime())
                  + ".csv")
 
     for user_id in users:
+        try:
+            if re.search(r"^\d{10}$", str(int(user_id))):
+                contact_menu = driver.find_element(
+                    By.ID,
+                    "new-menu"
+                )
+                contact_menu.click()
+                WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located(
+                        (By.CLASS_NAME,
+                         "btn-menu-overlay"))
+                )
+                new_chat = driver.find_element(
+                    By.XPATH,
+                    "//div[@class='btn-menu top-left active was-open']/div[3]"
+                )
+                new_chat.click()
+                WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//button[@class='btn-circle btn-corner z-depth-1 is-visible rp']"))
+                )
+                time.sleep(1)
+                add_contact = driver.find_element(
+                    By.XPATH,
+                    "//button[@class='btn-circle btn-corner z-depth-1 is-visible rp']/div[1]"
+                )
+                add_contact.click()
+                WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//div[@class='input-field input-field-name']"))
+                )
+                name_input = driver.find_element(
+                    By.XPATH,
+                    "//div[@class='input-field input-field-name']/div[1]"
+                )
+                name_input.send_keys(user_id)
+                WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//div[@class='input-field input-field-phone']"))
+                )
+                phone_input = driver.find_element(
+                    By.XPATH,
+                    "//div[@class='input-field input-field-phone']/div[1]"
+                )
+                phone_input.send_keys(user_id)
+                WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//button[@class='btn-primary btn-color-primary']"))
+                )
+                create_contact_button = contact_menu = driver.find_element(
+                    By.XPATH,
+                    "//button[@class='btn-primary btn-color-primary']"
+                )
+                create_contact_button.click()
+                WebDriverWait(driver, 15).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//div[@class='input-search']/input[1]"))
+                )
+                search_bar_input = driver.find_element(
+                    By.XPATH,
+                    "//div[@class='input-search']/input[1]"
+                )
+                search_bar_input.send_keys(user_id)
+                time.sleep(3)
+                WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (By.XPATH,
+                         "//ul[@id='contacts']/a[1]"))
+                )
+                requested_user = driver.find_element(
+                    By.XPATH,
+                    "//ul[@id='contacts']/a[1]"
+                )
+                requested_user.click()
 
-        if re.search(r"^\d{10}$", str(int(user_id))):
-            contact_menu = driver.find_element(
-                By.ID,
-                "new-menu"
-            )
-            contact_menu.click()
-            WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located(
-                    (By.CLASS_NAME,
-                     "btn-menu-overlay"))
-            )
-            new_chat = driver.find_element(
-                By.XPATH,
-                "//div[@class='btn-menu top-left active was-open']/div[3]"
-            )
-            new_chat.click()
-            WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     "//button[@class='btn-circle btn-corner z-depth-1 is-visible rp']"))
-            )
-            time.sleep(1)
-            add_contact = driver.find_element(
-                By.XPATH,
-                "//button[@class='btn-circle btn-corner z-depth-1 is-visible rp']/div[1]"
-            )
-            add_contact.click()
-            WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     "//div[@class='input-field input-field-name']"))
-            )
-            name_input = driver.find_element(
-                By.XPATH,
-                "//div[@class='input-field input-field-name']/div[1]"
-            )
-            name_input.send_keys(user_id)
-            WebDriverWait(driver, 5).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     "//div[@class='input-field input-field-phone']"))
-            )
-            phone_input = driver.find_element(
-                By.XPATH,
-                "//div[@class='input-field input-field-phone']/div[1]"
-            )
-            phone_input.send_keys(user_id)
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     "//button[@class='btn-primary btn-color-primary']"))
-            )
-            create_contact_button = contact_menu = driver.find_element(
-                By.XPATH,
-                "//button[@class='btn-primary btn-color-primary']"
-            )
-            create_contact_button.click()
-            WebDriverWait(driver, 15).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     "//div[@class='input-search']/input[1]"))
-            )
-            search_bar_input = driver.find_element(
-                By.XPATH,
-                "//div[@class='input-search']/input[1]"
-            )
-            search_bar_input.send_keys(user_id)
-            time.sleep(3)
-            WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH,
-                     "//ul[@id='contacts']/a[1]"))
-            )
-            requested_user = driver.find_element(
-                By.XPATH,
-                "//ul[@id='contacts']/a[1]"
-            )
-            requested_user.click()
+            else:
+                url = f'https://web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3D{user_id}'
+                driver.get(url)
 
-        else:
+        except Exception as e:
+            print("Getting user by id ", e)
             url = f'https://web.telegram.org/k/#?tgaddr=tg%3A%2F%2Fresolve%3Fdomain%3D{user_id}'
             driver.get(url)
 
@@ -139,8 +142,7 @@ def get_profile(users: List[str], driver=None):
             WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH,
                                             f"//div[@class='avatar avatar-like avatar-full"
-                                            + " avatar-gradient profile-avatars-avatar-first']"
-                                            + "/img[@class='avatar-photo']")))
+                                            + " avatar-gradient profile-avatars-avatar-first']")))
             img = driver.find_element(
                 By.XPATH,
                 f"//div[@class='avatar avatar-like avatar-full"
@@ -154,7 +156,7 @@ def get_profile(users: List[str], driver=None):
                 print("")
             time.sleep(.7)
             img.screenshot(f'{user_id}_{i}.png')
-        generate_file(user_id, file_name, profile)
+        generate_file(user_id, file_name, [profile['bio']])
 
 
 driver = use_driver()
@@ -169,4 +171,4 @@ driver.get(site)
  .until(EC.presence_of_element_located((By.CLASS_NAME, "input-search"))))
 
 print("Getting user's profile...")
-get_profile(["varyoe"], driver)
+get_profile(["9019051490"], driver)
